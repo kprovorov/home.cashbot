c8bd4c642fb37e7c488d9ee3542a826c2a3a2f32
Kirill Provorov <kirill@provorov.dev>
2023-04-07 00:38:31 +0200

    makeup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 export const metadata = {
   title: "Cashbot",
   description: "Plan your finances",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  themeColor: "#f4f4f5",
 };
 
 export default function RootLayout({
@@ -15,23 +17,6 @@ export default function RootLayout({
       lang="en"
       className="antialiased bg-gray-lightest text-base text-gray-dark h-full"
     >
-      <head>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
-        <meta name="theme-color" content="#f4f4f5" />
-        <meta
-          name="description"
-          content="Web site created using create-react-app"
-        />
-        <link rel="apple-touch-icon" href="/logo192.png" />
-
-        <link rel="manifest" href="/manifest.json" />
-        <title>Cashbot</title>
-      </head>
       <body className="h-full">{children}</body>
     </html>
   );
