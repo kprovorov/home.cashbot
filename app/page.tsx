5190cc7a5289b8c338b0415f8bad365dd51e7fbd
Kirill Provorov <kirill@provorov.dev>
2023-04-06 23:34:09 +0200

    form

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 import Scene from "./Scene";
 
 export default function Home() {
-  return <Scene />;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="h-40">
+        <Scene />
+      </div>
+      <h1 className="text-4xl font-bold">Cashbot</h1>
+      <form name="waitlist" method="POST" data-netlify="true">
+        <input type="email" id="email" name="email" />
+        <button type="submit">send</button>
+      </form>
+    </div>
+  );
 }
