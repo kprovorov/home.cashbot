d3f9d2ab14c84f68a37655f1479051dcb74ea13b
Kirill Provorov <kirill@provorov.dev>
2023-04-06 23:22:03 +0200

    makeup

--- app/Scene.tsx
+++ app/Scene.tsx
@@ -1,62 +1,30 @@
 "use client";
 
-import { Canvas, ThreeElements, useFrame, useLoader } from "@react-three/fiber";
-import { Suspense, useRef, useState } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { useRef } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Environment, OrbitControls } from "@react-three/drei";
 
-function Box(props: ThreeElements["mesh"]) {
-  const mesh = useRef<THREE.Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  useFrame((state, delta) => (mesh.current.rotation.x += delta));
-
-  const model = useLoader(GLTFLoader, "./3d/logo.gltf");
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <primitive object={model.scene} />
-      {/* <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} /> */}
-    </mesh>
-  );
-}
-
 const Model = () => {
   const mesh = useRef<THREE.Mesh>(null!);
-  const gltf = useLoader(GLTFLoader, "./3d/logo.gltf");
+  const object = useLoader(GLTFLoader, "./logo.glb");
 
-  const [scale, setScale] = useState(10);
-
-   useFrame((state, delta) => (mesh.current.rotation.y += delta));
+  useFrame((state, delta) => (mesh.current.rotation.y += delta));
 
   return (
-    <mesh
-      ref={mesh}
-      onPointerOver={(event) => setScale(12)}
-      onPointerOut={(event) => setScale(11)}
-    >
-      <primitive object={gltf.scene} scale={scale} />
+    <mesh ref={mesh}>
+      <primitive object={object.scene} scale={2} />
     </mesh>
   );
 };
 
 export default function Scene() {
   return (
-    <Canvas>
-      <Suspense fallback={null}>
-        <Model />
-        {/* <OrbitControls /> */}
-        <Environment preset="sunset" background />
-      </Suspense>
+    <Canvas camera={{ fov: 25, position: [0, 0, 9] }}>
+      <Model />
+      <OrbitControls enableZoom={false} enableRotate={false} />
+      <Environment preset="sunset" />
     </Canvas>
   );
 }
